2a81cfc26295105bb009c55871c7cb50309106fa
sharon <njerisharon84@gmail.com>
2025-09-21 13:53:32 +0300

    web-servers-and-wsgi

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request, Response
+
+@Request.application
+def application(request):
+    print(f"This web server is running at {request.remote_addr}")
+    
+    return Response("<b>A WSGI generated this response</b>")
+
+if __name__ == "__main__":
+    from werkzeug.serving import run_simple
+
+    run_simple(
+        hostname="localhost",
+        port=5555,
+        application=application
+    )
